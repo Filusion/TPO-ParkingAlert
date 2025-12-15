1ae8e343f9e5744311641c22aaae0487d5f3f0a9
Filusion <filip.vicoroski11@gmail.com>
2025-12-16 00:08:43 +0100

    test api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework.request import Request
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from api.models import User
+
 
 # Create your views here.
+
+class Test(APIView):
+
+    def get(self, request):
+        print('This is GET request.')
+        print(f'Request data: {request.data}')
+        print(f'Request query params: {request.query_params}')
+
+        users = list(User.objects.values())
+        return Response(users, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        print(f'Request data: {request.data}')
+        print(f'Request query params: {request.query_params}')
+
+        return Response(
+            {
+                "status": "ok",
+                "message": "POST request received",
+                "data": request.data
+            },
+            status=status.HTTP_200_OK
+        )
--- parking_alert/urls.py
+++ parking_alert/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import Test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/test/", Test.as_view(), name="test"),
 ]
