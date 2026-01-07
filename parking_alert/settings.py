5b8c2a1bd6b89f26727293ce13bb70f8fae80693
Filusion <filip.vicoroski11@gmail.com>
2026-01-07 23:18:46 +0100

    add Swagger permission

--- parking_alert/settings.py
+++ parking_alert/settings.py
@@ -116,6 +116,9 @@ ALLOWED_HOSTS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
 }
 
 
